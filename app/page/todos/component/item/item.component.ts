9458c9295653b526edc574a6dbcd07d857c237ea
Halyna Melishkevych <melishkevych@contractor.aveone.com>
2024-01-26 17:22:37 +0200

    Add "edit todo item" test

--- app/page/todos/component/item/item.component.ts
+++ app/page/todos/component/item/item.component.ts
@@ -4,12 +4,31 @@ import { step } from '../../../../../utils/stepDecorator'
 
 export class TodoItem extends Component {
   private readonly checkMark = this.page.locator('.toggle')
-  private readonly title = this.page.locator('.view label')
+  private readonly title = this.page.locator('.todo-list label')
   private readonly deleteButton = this.page.locator('.destroy')
+  private readonly editableItemInput = this.page.locator('.todo-list .edit')
+
+  @step()
+  async clickTitle (): Promise<void> {
+    await this.title.dblclick()
+  }
+
+  @step()
+  async updateTodo (text: string): Promise<void> {
+    await this.editableItemInput.fill(text)
+    await this.editableItemInput.press('Enter')
+  }
 
   @step()
   async expectLoaded (itemText: string): Promise<void> {
     await expect(this.checkMark).toBeVisible()
     await expect(this.title).toHaveText(itemText)
+  }
+
+  @step()
+  async expectEditable (): Promise<void> {
+    await expect(this.checkMark).toBeVisible({ visible: false })
+    await expect(this.deleteButton).toBeVisible({ visible: false })
+    await expect(this.title).toBeEditable()
   }
 }
---
+++ tests/updateTodos.test.ts
@@ -0,0 +1,27 @@
+import { test } from '@playwright/test'
+import { faker } from '@faker-js/faker'
+import { Application } from '../app'
+
+let todoItem: string
+test.beforeEach(async ({ page }) => {
+  todoItem = faker.lorem.sentence(3)
+})
+
+test.describe('Update Todo', () => {
+  test('Should allow the user to edit ToDo item', async ({ page }) => {
+    const app = new Application(page)
+    const updatedTodoItem = faker.lorem.sentence(3)
+
+    await app.todos.open()
+    await app.todos.expectLoaded()
+
+    await app.todos.addTodo(todoItem)
+    await app.todos.todoItem.clickTitle()
+
+    await app.todos.todoItem.expectEditable()
+
+    await app.todos.todoItem.updateTodo(updatedTodoItem)
+    await app.todos.todoItemFooter.expectLoaded()
+    await app.todos.todoItem.expectLoaded(updatedTodoItem)
+  })
+})
